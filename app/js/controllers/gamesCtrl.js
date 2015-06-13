95c04a162fdf6af4c6bc3965141c378e80290054
Mitko-Kerezov <d.kerezov@gmail.com>
2015-06-14 00:22:34 +0300

    Fix sonic game link

--- app/js/controllers/gamesCtrl.js
+++ app/js/controllers/gamesCtrl.js
@@ -3,7 +3,7 @@
 app.controller('gamesCtrl', ['$scope', 'identity','tripsService','$routeParams', function($scope, identity,tripService,$routeParams) {
     var games = [{title: "TicTacToe", url: "http://mitko-kerezov.github.io/TicTacToe/", description: "Tic Tac Toe game. You can play agains the CPU or against another player."}, 
                  {title: "Snake", url: "http://sttodorov.github.io/TheSnake/", description: "You've probably owned a Nokia phone - you know the drill."},
-                 {title: "Sonic", url: "#", description: "You've got 60 seconds to collect all coins, better hurry!"}
+                 {title: "Sonic", url: "http://mitko-kerezov.github.io/Sonic/", description: "You've got 70 seconds to collect all coins, better hurry!"}
                  ];
     $scope.games = games;
 }]);
