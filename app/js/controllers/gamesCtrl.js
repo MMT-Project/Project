d0fbc06b46dc12aff3b67c9d83d9035f0fa28671
Novkirishki <novkirishki@yahoo.com>
2015-07-06 14:47:05 +0300

    Added game images
    In games page

--- app/js/controllers/gamesCtrl.js
+++ app/js/controllers/gamesCtrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
 app.controller('gamesCtrl', ['$scope', 'identity','tripsService','$routeParams', function($scope, identity,tripService,$routeParams) {
-    var games = [{title: "TicTacToe", url: "#/gameTTT", description: "Tic Tac Toe game. You can play agains the CPU or against another player."}, 
-                 {title: "Snake", url: "#/gameSnake", description: "You've probably owned a Nokia phone - you know the drill."},
-                 {title: "Sonic", url: "#/gameSonic", description: "You've got 70 seconds to collect all coins, better hurry!"}
+    var games = [{title: "TicTacToe", url: "#/gameTTT", description: "Tic Tac Toe game. You can play agains the CPU or against another player.", img: 'img/TicTacToe.png'}, 
+                 {title: "Snake", url: "#/gameSnake", description: "You've probably owned a Nokia phone - you know the drill.", img: 'img/Snake.PNG'},
+                 {title: "Sonic", url: "#/gameSonic", description: "You've got 70 seconds to collect all coins, better hurry!", img: 'img/Sonic.PNG'}
                  ];
     $scope.games = games;
 }]);
